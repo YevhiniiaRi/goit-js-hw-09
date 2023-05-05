2f9d050b46fbe7c2630fca5333bbd81ffdf47780
YevheniiaRi <9263070@gmail.com>
2023-05-05 12:52:17 +0300

    Task-2

---
+++ src/js/02-timer.js
@@ -0,0 +1,62 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+
+const datePicker = document.getElementById('datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const daysElement = document.querySelector('[data-days]');
+const hoursElement = document.querySelector('[data-hours]');
+const minutesElement = document.querySelector('[data-minutes]');
+const secondsElement = document.querySelector('[data-seconds]');
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate < new Date()) {
+      window.alert('Please choose a date in the future');
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+};
+
+
+flatpickr(datePicker, options);
+
+let countdownIntervalId;
+
+
+startButton.addEventListener('click', () => {
+  const countdownDate = new Date(datePicker.value).getTime();
+
+
+  countdownIntervalId = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+ 
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  
+    daysElement.textContent = days.toString().padStart(2, '0');
+    hoursElement.textContent = hours.toString().padStart(2, '0');
+    minutesElement.textContent = minutes.toString().padStart(2, '0');
+    secondsElement.textContent = seconds.toString().padStart(2, '0');
+
+
+    if (distance <= 0) {
+      clearInterval(countdownIntervalId);
+    }
+  }, 1000);
+});
